be52cabd01d8c97c4c778da95c07c56599e68f29
BuyBackGit <info.buybacktoken@gmail.com>
2021-10-20 23:37:32 +0100

    homepage commit

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const appId = "bT4pAMHLfPBXlo8Y7QMYGQDS9NBZOmPcigHV5gDY";
 Moralis.start({ serverUrl, appId });
 
 
-let homepage ="http://127.0.0.1:5500/index.html"
+let homepage ="https://buybackgit.github.io/bbtdashboard/"
 
 if (Moralis.User.current() == null && window.location.href != homepage) {
     document.querySelector('body').style.display ='none';
